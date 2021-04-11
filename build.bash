cf70bc50fa687d9adc9b5cbd30aa3a7bf0626b75
Blaine Simpson <blaine.simpson@admc.com>
2021-04-11 13:21:34 -0400

    generalizations

--- build.bash
+++ build.bash
@@ -6,6 +6,12 @@ PROGNAME="${0##*/}"
 set +u
 shopt -s xpg_echo
 
+unset DO_RM
+[ $# -gt 0 ] && [ "$1" = -r ] && {
+    DO_RM=true
+    shift
+}
+
 [ $# -ne 1 ] && {
     echo "SYNTAX:  $PROGNAME MainClass
 Use env vars to override these defaults:
@@ -17,22 +23,30 @@ DEST_URL: file:/tmp/repo
 MAINCLASS="$1"; shift
 
 [ -n "$DEST_URL" ] || DEST_URL=file:/tmp/repo
-[ -n "$VERSION" ] || VERSION=0.1a
+[ -n "$VERSION" ] || VERSION=z01
 
 Abort() {
     echo "Aborting $PROGNAME:  $*" 1>&2
     exit 1
 }
 
+PROJECT_NAME=gradle-ivyxml-plugin
 
+[ -n "$DO_RM" ] && {
+    case "$DEST_URL" in file:*);; *)
+        echo 'Can only use -r switch with file DEST_URLs' 1>&2; exit 3;;
+    esac
+    echo "Wiping ${DEST_URL#file:}..."
+    rm -rf "${DEST_URL#file:}"
+}
 gradlew -Pmc="$MAINCLASS" -Purl="$DEST_URL" clean build uploadArchives &&
-echo -n "From build/libs/repotst-$VERSION.jar:  " &&
-java -jar build/libs/repotst-$VERSION.jar &&
+echo -n "From build/libs/${PROJECT_NAME}-$VERSION.jar:  " &&
+java -jar build/libs/${PROJECT_NAME}-$VERSION.jar &&
 case "$DEST_URL" in file:*)
     BASEDIR=${DEST_URL#file:}
     [ -d "$BASEDIR" ] || Abort "Target repost dir missing: BASEDIR"
-    [ -f "$BASEDIR/repotst/$VERSION/repotst-$VERSION.jar" ] ||
-    Abort "Reposed base jar missing: $BASEDIR/repotst/$VERSION/repotst-$VERSION.jar"
-    echo -n "From $BASEDIR/repotst/$VERSION/repotst-$VERSION.jar:  " &&
-    java -jar "$BASEDIR/repotst/$VERSION/repotst-$VERSION.jar"
+    [ -f "$BASEDIR/com/admc/$PROJECT_NAME/$VERSION/${PROJECT_NAME}-$VERSION.jar" ] ||
+    Abort "Reposed base jar missing: $BASEDIR/com/admc/$PROJECT_NAME/$VERSION/${PROJECT_NAME}-$VERSION.jar"
+    echo -n "From $BASEDIR/com/admc/$PROJECT_NAME/$VERSION/${PROJECT_NAME}-$VERSION.jar:  " &&
+    java -jar "$BASEDIR/com/admc/$PROJECT_NAME/$VERSION/${PROJECT_NAME}-$VERSION.jar"
 ;; esac
